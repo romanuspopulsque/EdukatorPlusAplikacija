4204e0568c55989480376e154399721e8444950c
Roman Simunovic <romansimunovic21@gmail.com>
2025-06-25 21:35:25 +0200

    Create data.sql

---
+++ Edukatorplus-aplikacija-IKI-main/EdukatorPlus/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- Ubacivanje polaznika
+INSERT INTO polaznici (id, ime, prezime, email) VALUES (1, 'Ivan', 'Ivić', 'ivan.ivic@example.com');
+INSERT INTO polaznici (id, ime, prezime, email) VALUES (2, 'Ana', 'Anić', 'ana.anic@example.com');
+
+-- Ubacivanje radionica
+INSERT INTO radionice (id, naziv, datum) VALUES (1, 'React osnove', '2025-07-01');
+INSERT INTO radionice (id, naziv, datum) VALUES (2, 'Spring Boot napredno', '2025-08-15');
+
