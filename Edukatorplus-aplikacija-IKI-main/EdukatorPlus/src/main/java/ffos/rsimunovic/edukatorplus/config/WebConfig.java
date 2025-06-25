ad3a8dc42a292173f8ede166d04f6d3f5e4af392
Roman Simunovic <romansimunovic21@gmail.com>
2025-06-25 23:37:06 +0200

    Update WebConfig.java

--- Edukatorplus-aplikacija-IKI-main/EdukatorPlus/src/main/java/ffos/rsimunovic/edukatorplus/config/WebConfig.java
+++ Edukatorplus-aplikacija-IKI-main/EdukatorPlus/src/main/java/ffos/rsimunovic/edukatorplus/config/WebConfig.java
@@ -5,12 +5,13 @@ import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
+@Override
+public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("https://edukatorplusaplikacija-3.onrender.com", "http://localhost:3000")
-      .allowedMethods("*")
-      .allowedHeaders("*")
-      .allowCredentials(true);
-  }
+        .allowedOrigins("http://localhost:3000", "https://edukatorplusaplikacija-3.onrender.com")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(3600);
+}
 }
