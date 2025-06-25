e3fe3e3c492c2f894d579e9ab6927eb3cac30d9c
Roman Simunovic <romansimunovic21@gmail.com>
2025-06-25 22:55:28 +0200

    webconfig

---
+++ Edukatorplus-aplikacija-IKI-main/EdukatorPlus/src/main/java/ffos/rsimunovic/edukatorplus/config/WebConfig.java
@@ -0,0 +1,21 @@
+package ffos.rsimunovic.edukatorplus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://edukatorplusaplikacija-3.onrender.com") // tvoj frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
