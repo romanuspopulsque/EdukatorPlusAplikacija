e885b9888f777cdb30d25fa96fdf37dec1a330b1
Roman Simunovic <romansimunovic21@gmail.com>
2025-06-26 00:31:19 +0200

    Update EdukatorPlusApplication.java

--- Edukatorplus-aplikacija-IKI-main/EdukatorPlus/src/main/java/ffos/rsimunovic/edukatorplus/EdukatorPlusApplication.java
+++ Edukatorplus-aplikacija-IKI-main/EdukatorPlus/src/main/java/ffos/rsimunovic/edukatorplus/EdukatorPlusApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Start {
+public class EdukatorPlusApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Start.class, args);
+        SpringApplication.run(EdukatorPlusApplication.class, args);
     }
 }
